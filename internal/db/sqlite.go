3d5d70f9cbfd757cb4d16eda8328ee31a2b334a8
sanzhh <sanzhar.maratov@proton.me>
2023-09-10 14:23:40 +0600

    [added] sqlite db connection

---
+++ internal/db/sqlite.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"database/sql"
+
+	"github.com/sanzhh/todo/internal/lib/e"
+)
+
+func NewSQLite(filepath string) (_ *sql.DB, err error) {
+	var (
+		errorMessage = "sqlite connection failed"
+	)
+
+	defer func() { err = e.WrapS(errorMessage, err) }()
+
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
