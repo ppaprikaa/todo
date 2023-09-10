0b0a8b81d48f16fed88eb482dbae4b73c1c9d6a6
sanzhh <sanzhar.maratov@proton.me>
2023-09-10 15:41:05 +0600

    [changed] todo model

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -1,8 +1,11 @@
 package models
 
+import "time"
+
 type Todo struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Done        bool   `json:"done"`
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Date        time.Time `json:"date"`
+	Done        bool      `json:"done"`
 }
